d6532a14f7544439a4123681459f7df72817cc1c
PauloCamargos <paulo.camargos@ufu.br>
2018-08-17 17:39:13 -0300

    cadastro funcionando

--- ProntuarioEletronico/peufu.sql
+++ ProntuarioEletronico/peufu.sql
@@ -4,12 +4,87 @@
  */
 
 -- CRIANDO A TABELA 'users'
-CREATE TABLE users (
+-- Campos obrigatorios: nome_completo, especialidade, crm
+CREATE TABLE usuarios (
     id SERIAL PRIMARY KEY,
-    usuario VARCHAR(32),
-    senha VARCHAR(16)
-    );
+    nome_completo VARCHAR(128) NOT NULL,
+    idade INT NOT NULL,
+    endereco VARCHAR(512),
+    especialidade_id INT NOT NULL,
+    crm INT UNIQUE NOT NULL,
 
--- INSERINDO VALORES NA TABELA 'users'
-INSERT INTO users (usuario, senha) 
-VALUES ('paulo@ufu.br', '1234');
+    CHECK(idade>=18),
+    FOREIGN KEY (especialidade_id) REFERENCES especialidades(id)
+    );
+    
+-- CRIANDO A TABELA 'especialidades'
+CREATE TABLE especialidades (
+	id SERIAL PRIMARY KEY,
+    especialidade VARCHAR(64)
+);
+-- INSERINDO VALORES NA TABELA 'especialidades'
+INSERT INTO especialidades (especialidade) VALUES ("Acupuntura"),
+      ("Alergia e Imunologia"),
+      ("Anestesiologia"),
+      ("Angiologia"),
+      ("Cancerologia"),
+      ("Cardiologia"),
+      ("Cirurgia Cardiovascular"),
+      ("Cirurgia da Mão"),
+      ("Cirurgia de Cabeça e Pescoço"),
+      ("Cirurgia do Aparelho Digestivo"),
+      ("Cirurgia Geral"),
+      ("Cirurgia Pediátrica"),
+      ("Cirurgia Plástica"),
+      ("Cirurgia Torácica"),
+      ("Cirurgia Vascular"),
+      ("Clínica Médica"),
+      ("Coloproctologia"),
+      ("Dermatologia"),
+      ("Endocrinologia e Metabologia"),
+      ("Endoscopia"),
+      ("Gastroenterologia"),
+      ("Genética Médica"),
+      ("Geral"),
+      ("Geriatria"),
+      ("Ginecologia e Obstetrícia"),
+      ("Hematologia e Hemoterapia"),
+      ("Homeopatia"),
+      ("Infectologia"),
+      ("Mastologia"),
+      ("Medicina de Família e Comunidade"),
+      ("Medicina do Trabalho"),
+      ("Medicina de Tráfego"),
+      ("Medicina Esportiva"),
+      ("Medicina Física e Reabilitação"),
+      ("Medicina Intensiva"),
+      ("Medicina Legal e Perícia Médica"),
+      ("Medicina Nuclear"),
+      ("Medicina Preventiva e Social"),
+      ("Nefrologia"),
+      ("Neurocirurgia"),
+      ("Neurologia"),
+      ("Nutrologia"),
+      ("Oftalmologia"),
+      ("Ortopedia e Traumatologia"),
+      ("Otorrinolaringologia"),
+      ("Patologia"),
+      ("Patologia Clínica/Medicina Laboratorial"),
+      ("Pediatria"),
+      ("Pneumologia"),
+      ("Psiquiatria"),
+      ("Radiologia e Diagnóstico por Imagem"),
+      ("Radioterapia"),
+      ("Reumatologia"),
+      ("Urologia");
+
+-- CRIANDO A TABELA login
+CREATE TABLE login(
+    id SERIAL PRIMARY KEY,
+    usuario_id INT,
+    usuario VARCHAR(64) NOT NULL,
+    senha VARCHAR(512) NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+    )
+
+    
--- ProntuarioEletronico/src/java/cadastrar/Cadastrar.java
+++ ProntuarioEletronico/src/java/cadastrar/Cadastrar.java
@@ -5,11 +5,18 @@
  */
 package cadastrar;
 
+import com.database.ConexaoDatabase;
 import com.seguranca.BCrypt;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -42,27 +49,75 @@ public class Cadastrar extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 //        DECLARANDO VARIAVEIS
-        String nome_completo, endereco, usuario, senha, senha_encriptada = null;
-        int idade;
-         MessageDigest md;
+        Connection conexao = ConexaoDatabase.getConnection();
+        Statement st;
+        ResultSet rs;
+        String query_usuarios, query_login, nome_completo, endereco, usuario,
+                senha, senha_encriptada = null;
+        int idade, especialidade, crm;
 
-//        RECOLHENDO VALORES DO FORMULARIO
-        nome_completo = request.getParameter("nome");
-        if(!request.getParameter("idade").equals(""))
+        System.out.println("INICIANDO SERVLET");
+            
+//        RECOLHENDO VALORES DO FORMULARIO | VERIFICANDO SE nome e crm NÃO SÃO VAZIOS
+        if (!request.getParameter("nome").isEmpty() 
+                && !request.getParameter("crm").isEmpty() 
+                && !request.getParameter("idade").isEmpty()) {
             idade = Integer.parseInt(request.getParameter("idade"));
-        endereco = request.getParameter("endereco");
-        usuario = request.getParameter("usuario");
-        senha = request.getParameter("senha");
+            // CHECANDO SE A IDADE NÃO E VAZIA E MAIOR QUE 18
+            if (idade > 18) {
+                nome_completo = request.getParameter("nome");//NOME   
+                endereco = request.getParameter("endereco");
+                usuario = request.getParameter("usuario");
+                especialidade = Integer.parseInt(request.getParameter("especialidade"));
+                crm = Integer.parseInt(request.getParameter("crm"));
+                senha = request.getParameter("senha");
+                senha_encriptada = encriptar_senha(senha);
 
-        String senha_banco = "1234";
-        senha_encriptada = BCrypt.hashpw(senha, BCrypt.gensalt());
-        
-        if(BCrypt.checkpw(senha_banco, senha_encriptada)){
-            System.out.println("BATE");
-        }else{
-            System.out.println("NÃO NÃO");
+                //Inserindo usuario na tabela usuarios
+                query_usuarios = "INSERT INTO usuarios (nome_completo, idade, endereco, especialidade_id, crm) VALUES ("
+                        + "'" + nome_completo + "',"
+                        + idade + ","
+                        + "'" + endereco + "',"
+                        + "'" + especialidade + "',"
+                        + "" + crm + ")";
+
+                //Buscando o usuario na tabela usuarios
+                query_login = "SELECT id FROM usuarios WHERE crm =" + crm;
+
+                //Inserindousuario no 
+                try {
+                    st = conexao.createStatement();
+                    st.executeUpdate(query_usuarios);
+                    rs = st.executeQuery(query_login);
+                    rs.next();
+                    query_login = "INSERT INTO login (usuario_id, usuario, senha) VALUES ("
+                            + "'" + rs.getString("id") + "'," 
+                            + "'" + usuario + "',"
+                            + "'" + senha_encriptada + "');";
+                    st.executeUpdate(query_login);
+                    System.out.println("[OK] USUÁRIO INSERIDO COM SUCESSO");
+                } catch (SQLException ex) {
+                    Logger.getLogger(Cadastrar.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            } else {
+                // TODO: RETORNAR ERRO IDADE
+            }
         }
-        
 
+//
+//        String senha_banco = "1234";
+//        senha_encriptada = BCrypt.hashpw(senha, BCrypt.gensalt());
+//        
+//        if(BCrypt.checkpw(senha_banco, senha_encriptada)){
+//            System.out.println("BATE");
+//        }else{
+//            System.out.println("NÃO NÃO");
+//        }
+    }
+
+    private String encriptar_senha(String senha) {
+        String senha_encriptada = BCrypt.hashpw(senha, BCrypt.gensalt());
+        return senha_encriptada;
     }
 }
